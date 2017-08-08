7d6659de35ffb1b0f85dc4f29aebc9b299793a77
Sepand Ansari <sepans@sepans.com>
2017-08-08 11:09:34 -0400

    hide history

--- src/components/HistoryPanel.js
+++ src/components/HistoryPanel.js
@@ -6,7 +6,7 @@ const HistoryPanel = (props) => {
 			<li key={d.title}>{d.title}</li>
 		))
 	return (
-			<div  className="history">
+			<div  className="history" style={{opacity: props.history.length> 1 ? 1 : 0}}>
 				<div className="label">Navigation hisotry:</div>
 				<ul style={{transform: props.history.length < 2 ? 'translateY(100%)' : ''}}>
 					{historyList}
