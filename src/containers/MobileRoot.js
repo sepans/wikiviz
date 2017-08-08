699d75e9fabdbede7a7cfe974cfbcfc20db41952
Sepand Ansari <sepans@sepans.com>
2017-08-08 13:40:50 -0400

    fix mobile styles

--- src/containers/MobileRoot.js
+++ src/containers/MobileRoot.js
@@ -4,14 +4,14 @@ import '../styles/MobileRoot.css'
 const MobileRoot = (props) => {
 	return (
 		<div className="mobile">
-			<span className="ffl-logo"><a href="http://fastforwardlabs.com" target="_blank">Fast forward labs</a></span>
-			<h1 className="title">Encartopedia</h1>
-			<h2 className="subtitle">  Wikipedia with a Map</h2>
+			<span className="mobileffl-logo"><a href="http://fastforwardlabs.com" target="_blank">Fast forward labs</a></span>
+			<h1 className="mobiletitle">Encartopedia</h1>
+			<h2 className="mobilesubtitle">  Wikipedia with a Map</h2>
 			
 			<p><b>Encartopedia</b> helps locating yourself, or to be more precise, 
 						locate the subject matter of your curiosity within the universe of Wikipedia articles.</p>
-			<img className="screenshot" src="img/screen.png"/>
-			<div className="checkback">
+			<img className="mobilescreenshot" src="img/screen.png"/>
+			<div className="mobilecheckback">
 			 Please check back on a large screen.
 			</div>
 		</div>
